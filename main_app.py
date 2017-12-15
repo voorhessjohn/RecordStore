3751210dc521c3b3cf599bac63c472b40924b7c6
johnglennvoorhess <voorhessjohn@gmail.com>
2017-12-15 13:19:20 -0500

    added user registration

--- main_app.py
+++ main_app.py
@@ -10,7 +10,8 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager, Shell
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FileField, IntegerField, DateField, FloatField, BooleanField
-from wtforms.validators import Required
+from wtforms.validators import Required, Email
+from wtforms.fields.html5 import EmailField
 from flask_sqlalchemy import SQLAlchemy
 import random
 from flask_migrate import Migrate, MigrateCommand # needs: pip/pip3 install flask-migrate
@@ -18,6 +19,26 @@ from flask_mail import Mail, Message
 from threading import Thread
 from werkzeug import secure_filename
 from flask_bootstrap import Bootstrap
+
+# Get some data from another source (an API, BeautifulSoup)
+# At least 4 view functions (not counting error handling) [DONE]
+# 	index
+# 	wishlist_view
+# 	record_view
+# 	upload
+# At least 2 error handling view functions (404 and whatever other real error you want)
+# At least 3 models (database tables)
+# At least 1 one-to-many relationship
+# At least 1 many-to-many relationship with association table
+# At least 2 get_or_create functions to deal with entering data into a database
+# At least 1 form using WTForms
+# At least 2 dynamic links, which can be covered byoa href tags that send data that is processed by the end URL
+# 	using url_for
+# 	using redirect
+# (200 points of the 2500)Use at least one flask extension so that it works:
+# Could be Flask email
+# user authentication
+# file upload
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -45,7 +66,7 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand) 
 mail = Mail(app) 
 
-
+sales_order_record = db.Table('sales_order_record',db.Column('sales_order_id',db.Integer, db.ForeignKey('sales_orders.id')),db.Column('record_id',db.Integer, db.ForeignKey('records.id')))
 
 class User(db.Model):
     __tablename__ = "users"
@@ -103,6 +124,11 @@ class RecordForm(FlaskForm):
 	price = FloatField("Price")
 	submit = SubmitField('Submit')
 
+class registrationForm(FlaskForm):
+	userName = StringField("user name", validators=[Required()])
+	email = EmailField("email", validators=[Required()])
+	submit = SubmitField('Submit')
+
 class UploadForm(FlaskForm):
     file = FileField()
 
@@ -157,27 +183,64 @@ def get_or_create_sales_order_line(
 	catalog_no, 
 	user_id
 	):
-    sales_order_line = db_session.query(Sales_Order).filter_by(catalog_no=catalog_no).first()
+    sales_order_line = db_session.query(Sales_Order).filter_by(catalog_no=catalog_no).filter_by(user_id=user_id).first()
     user = db_session.query(User).filter_by(id=user_id).first()
     if sales_order_line:
         return sales_order_line
     else:
         sales_order_line = Sales_Order(
         	catalog_no=catalog_no,
-        	user_id=user.id
+        	user_id=user_id
         	)
         db_session.add(sales_order_line)
         db_session.commit()
+        flash("item added.")
         return sales_order_line
+
+def get_or_create_user(
+				db_session,
+				userName,
+            	email 
+            	):
+	user = db.session.query(User).filter_by(email=email).first()
+	if user:
+		return user
+	else:
+		user = User(
+			userName=userName,
+			email=email
+			)
+		db_session.add(user)
+		db_session.commit()
+		flash("user added.")
+		return user
+	
 
 def get_number_of_records():
 	records = Record.query.all()
 	num_records = len(records)
 	return num_records
 
+## Error handlers
+@app.errorhandler(404)
+def four_oh_four(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+	num_records = get_number_of_records()
+	all_records = []
+	records = Record.query.all()
+	for r in records:
+		all_records.append((r.title, r.artist, r.catalog_no))
+	return render_template('index.html',all_records=all_records,num_records=num_records)
+
+@app.route('/add_records_to_db', methods=['GET','POST'])
+def add_records_to_db():
     num_records = get_number_of_records()
     form = RecordForm()
     if form.validate_on_submit():
@@ -201,16 +264,7 @@ def index():
             	form.collection_notes.data,
             	form.price.data
             	)
-    return render_template('index.html', form=form,num_records=num_records)
-
-@app.route('/all_records')
-def see_all():
-	num_records = get_number_of_records()
-	all_records = []
-	records = Record.query.all()
-	for r in records:
-		all_records.append((r.title, r.artist, r.catalog_no))
-	return render_template('all_records.html',all_records=all_records,num_records=num_records)
+    return render_template('add_records_to_db.html', form=form,num_records=num_records)
 
 @app.route('/record_view/<catalog_no>', methods=['GET', 'POST'])
 def record_view(catalog_no):
@@ -227,32 +281,61 @@ def record_view(catalog_no):
 	record_dict['collection_notes']=record.collection_notes
 	record_dict['price']=record.price
 	if form.validate_on_submit():
-		if db.session.query(Sales_Order).filter_by(catalog_no=catalog_no).first(): 
+		user_id = form.user_id.data
+		user = db.session.query(User).filter_by(id=user_id).first()
+		if db.session.query(Sales_Order).filter_by(catalog_no=catalog_no).filter_by(user_id=user_id).first(): 
 			flash("You've already saved a record with that catalog number!")
+		elif not user:
+			flash("User is not registered")
 		else:
-			user_id = form.user_id.data
-			get_or_create_sales_order_line(
+			new_sales_order_line = get_or_create_sales_order_line(
 				db.session,
 				catalog_no,
             	user_id 
             	)
-			return url_for(wishlist_view(user_id))
+			user_id = new_sales_order_line.user_id
 	return render_template('record_view.html', record_dict=record_dict, form=form, num_records=num_records)
 
 @app.route('/wishlist_view/<user_id>', methods=['GET','POST'])
 def wishlist_view(user_id):
 	num_records=get_number_of_records()
 	list_of_sales_order_lines = []
+	
 	sales_order_lines = Sales_Order.query.filter_by(user_id=user_id).all()
+	if not sales_order_lines:
+		flash("No wishlist items for that user")
+		return url_for(index)
+		
 	for line in sales_order_lines:
 		list_of_sales_order_lines.append(line)
 	return render_template(
 		'wishlist_view.html', 
 		user_id=user_id, 
 		list_of_sales_order_lines=list_of_sales_order_lines,
-		num_records=num_records,
+		num_records=num_records
 		)
 
+@app.route('/register', methods=['GET','POST'])
+def register():
+	num_records=get_number_of_records()
+	form=registrationForm()
+	if form.validate_on_submit():
+		userName = form.userName.data
+		email = form.email.data
+		user = db.session.query(User).filter_by(email=email).first()
+		if user:
+			user_id = user.id
+			flash("There's already a user with that email.")
+			return render_template('wishlist_view.html', user_id=user_id)
+		else:
+			new_user = get_or_create_user(
+				db.session,
+				userName,
+            	email 
+            	)
+			user_id = new_user.id
+			# return url_for(wishlist_view)
+	return render_template('register.html', form=form, num_records=num_records)
 
 
 
